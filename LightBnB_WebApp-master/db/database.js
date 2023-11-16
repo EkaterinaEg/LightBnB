3f6d5f45dd69b500d997c87de1f36afa032ff109
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-11-15 23:19:06 -0500

    refactor addProperty function to create new property

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -35,14 +35,6 @@ const getUserWithEmail = function (email) {
     .catch((err) => {
       console.log("error ", err.message);
     });
-  // let resolvedUser = null;
-  // for (const userId in users) {
-  //   const user = users[userId];
-  //   if (user && user.email.toLowerCase() === email.toLowerCase()) {
-  //     resolvedUser = user;
-  //   }
-  // }
-  // return Promise.resolve(resolvedUser);
 };
 
 /**
@@ -68,7 +60,6 @@ const getUserWithId = function (id) {
     .catch((err) => {
       console.log("error ", err.message);
     });
-  // return Promise.resolve(users[id]);
 };
 
 /**
@@ -91,11 +82,6 @@ const addUser = function (user) {
     .catch((err) => {
       console.log("error ", err.message);
     });
-
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
 };
 
 /// Reservations
@@ -125,7 +111,6 @@ const getAllReservations = function (guest_id, limit = 10) {
     .catch((err) => {
       console.log("error ", err.message);
     });
-  // return getAllProperties(null, 2);
 };
 
 /// Properties
@@ -191,11 +176,6 @@ const getAllProperties = function (options, limit = 10) {
     .catch((err) => {
       console.log(err.message);
     });
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
 };
 
 /**
@@ -204,10 +184,49 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(
+      `INSERT INTO properties(
+      owner_id, title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    )
+     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+     RETURNING *
+     `,
+      [
+        property.owner_id,
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        property.cost_per_night,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+        property.country,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms,
+      ]
+    )
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log("error ", err.message);
+    });
 };
 
 module.exports = {
@@ -218,11 +237,3 @@ module.exports = {
   getAllProperties,
   addProperty,
 };
-// SELECT properties.*, AVG(property_reviews.rating) as average_rating
-//   FROM properties
-//   JOIN property_reviews ON properties.id = property_id
-//   WHERE city LIKE '%ancouver%' AND cost_per_night > 12300
-//   GROUP BY properties.id
-//    HAVING AVG(property_reviews.rating) > 2
-//   ORDER BY cost_per_night
-//   LIMIT 10;
